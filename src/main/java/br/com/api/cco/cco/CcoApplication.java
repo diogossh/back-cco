6854fb2fa6b1a82beae32583ef52d13799b71e70
diogossh3 <diogohdsh@gmail.com>
2024-01-30 10:23:31 -0300

    primeiro envio

---
+++ src/main/java/br/com/api/cco/cco/CcoApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.cco.cco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcoApplication.class, args);
+	}
+
+}
